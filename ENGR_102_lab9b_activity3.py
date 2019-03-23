7c650b37f977add8e6a5057567bdd68414595730
djohnson135 <dathanljohnson135@gmail.com>
2019-03-23 17:36:01 -0500

    activity 3 does not work well in vs

---
+++ ENGR_102_lab9b_activity3.py
@@ -0,0 +1,48 @@
+"""
+Name : Dathan Johnson
+Section : 409
+Assignment : lab 9b - 3
+Date : 3-23-19
+
+Each month, you should calculate the amount remaining on the loan by first applying the
+monthly payment (reducing the loan value), then increasing the loan amount by 1/12 of the
+annual interest rate.
+"""
+import csv
+# inputs for values
+loan = float(input('amount of loan: '))
+intrate = float(input('annual interst rate: '))
+monthdue = float(input('amount payed monthly: '))
+name = input('name of file: ')
+#name of file is name with the added by .csv
+filename = name + '.csv'
+fid = open(filename,'w')
+#writer function for the .csv
+writer = csv.writer(fid)
+#create list to add to file
+header = ['month', 'total interest', 'remaining loan amount']
+writer.writerow(header)
+#the initial values
+start = ['0','0', str(loan)]
+writer.writerow(start)
+#initialize variables
+total_int = 0
+month = 0
+#loop till loan is depleted
+while loan > 0:
+    if month == 30: #if the month is 30 break out of loop to avoid an infinite loop
+        break
+    else:
+        loan -= monthdue #calculate loan
+        preloan = loan
+        loan += preloan * (intrate/100) * 1/12
+        month += 1
+        my_list = [str(month), str(total_int), str(loan)] #list of values
+        writer.writerow(my_list)
+        if (loan - preloan) < 0:
+            break
+        else:
+            interest = loan - preloan
+            total_int += interest #add the total interest
+#close file
+fid.close()
