f49fac4273c8e80df4fc11883ae42d04b221415b
djohnson135 <dathanljohnson135@gmail.com>
2019-03-23 14:19:41 -0500

    activity 2

---
+++ ENGR_102_lab9b_activity2.py
@@ -0,0 +1,34 @@
+"""
+Name : Dathan Johnson
+Section : 409
+Assignment : lab 9b - 2
+Date : 3-23-19
+
+"""
+
+#open celsius file
+try:
+    fid = open('Celsius.txt','r')
+except FileNotFoundError:
+    print('no file')
+#create a new file for fahrenheit
+farenfile = 'Fahrenheit.txt'
+faren = open(farenfile, 'w') #open the file on W
+
+#initialize list
+data = []
+#make a list for celsius
+x = fid.readlines()
+#loop to append values to list
+for i in x:
+    val = int(i)
+    data.append(val)
+#loop to convert values to fahrenheit
+for i in data:
+    fahrenheit = (i * (9/5)) + 32
+    faren.write(str(fahrenheit) + '\n' )
+
+
+#close files
+fid.close()
+faren.close()
